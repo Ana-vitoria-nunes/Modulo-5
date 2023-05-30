e547ad3ca66fa77747ea0379fad1358fb1ea99ad
Ana.Vitoria.Nunes <ana.nunes@zup.com.br>
2023-05-30 18:49:15 -0300

    exercicios

---
+++ Exercicios/src/Exercicio1/Main.java
@@ -0,0 +1,23 @@
+package Exercicio1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numeros = new ArrayList();
+        numeros.add(1);
+        numeros.add(5);
+        numeros.add(5);
+        numeros.add(6);
+        numeros.add(7);
+        numeros.add(8);
+        numeros.add(8);
+        numeros.add(8);
+
+        System.out.println(numeros);
+    }
+}
---
+++ Exercicios/src/Exercicio2/Main.java
@@ -0,0 +1,22 @@
+package Exercicio2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> numero = new HashSet();
+        numero.add(1);
+        numero.add(5);
+        numero.add(5);
+        numero.add(6);
+        numero.add(7);
+        numero.add(8);
+        numero.add(8);
+        numero.add(8);
+        System.out.println(numero);
+    }
+}
